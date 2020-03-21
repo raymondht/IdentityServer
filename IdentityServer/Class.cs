6a12350f36e74a3c4a4678d1c054e883bc031ec2
Erayus <raymondhieutran@gmail.com>
2020-03-22 00:31:21 +1100

    Add project files.

---
+++ IdentityServer/Class.cs
@@ -0,0 +1,28 @@
+﻿using IdentityModel;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public class IdentityServer
+    {
+        public IEnumerable<ApiResource> GetApis() =>
+            new List<ApiResource> 
+            {
+                new ApiResource("ApiOne"),
+            };
+
+        public static IEnumerable<Client> GetClients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client_id",
+                    ClientSecrets = { new Secret("client_secret".ToSha256()) }
+                }
+            };
+    }
+}
